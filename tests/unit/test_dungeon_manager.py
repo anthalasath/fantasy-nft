b4463713eb1ae27701028225e34100e2e6fb8e8a
Anthalasath <chapchico@live.com>
2022-03-01 23:48:19 +0100

    add test for retiring dungeon

--- tests/unit/test_dungeon_manager.py
+++ tests/unit/test_dungeon_manager.py
@@ -14,8 +14,7 @@ def test_create_dungeon_without_ether():
     with pytest.raises(exceptions.VirtualMachineError):
         dm.createDungeon({"from": account, "value": 0})
     dungeon = dm.dungeons(account.address)
-    assert dungeon[0] == ZERO_ADDRESS
-    assert dungeon[1] == 0
+    assert_dungeon_doesnt_exists(dungeon)
 
 def test_create_dungeon_with_ether():
     if network.show_active() not in LOCAL_BLOCKAIN_ENVIRONMENTS:
@@ -49,8 +48,39 @@ def test_create_dungeon_when_one_already_exists():
     with pytest.raises(exceptions.VirtualMachineError):
         dm.createDungeon({"from": account, "value": 1})
 
+def test_retire_dungeon_when_no_dungeon_active():
+    if network.show_active() not in LOCAL_BLOCKAIN_ENVIRONMENTS:
+        pytest.skip()
+    fantasy = deploy_fantasy()
+    dm = deploy_dungeon_manager(fantasy_address=fantasy.address)
+    account = get_account()
+
+    with pytest.raises(exceptions.VirtualMachineError):
+        dm.retireDungeon({"from": account})
+
+def test_retire_dungeon_when_dungeon_active():
+    if network.show_active() not in LOCAL_BLOCKAIN_ENVIRONMENTS:
+        pytest.skip()
+    fantasy = deploy_fantasy()
+    dm = deploy_dungeon_manager(fantasy_address=fantasy.address)
+    account = get_account()
+    treasure = Web3.toWei(1, "ether")
+    dm.createDungeon({"from": account, "value": treasure})
+    balance_before_retiring_dungeon = account.balance()
+    
+    dm.retireDungeon({"from": account})
+
+    assert dm.balance() == 0
+    assert account.balance() > balance_before_retiring_dungeon # TODO better check by computing tx fee ?
+
+    assert_dungeon_doesnt_exists(dm.dungeons(account.address))
+    
+
 def assert_adventuring_party_is_empty(party):
     assert party[0] == ZERO_ADDRESS
     assert len(party[1]) == 0
     assert party[2] == 0
 
+def assert_dungeon_doesnt_exists(dungeon):
+    assert dungeon[0] == ZERO_ADDRESS
+    assert dungeon[1] == 0

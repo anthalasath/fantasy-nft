6cfc83735f5216105e6a97689d3c21ffc1ae1f6f
Anthalasath <chapchico@live.com>
2022-03-01 20:28:52 +0100

    add test for creating dungeons

--- tests/unit/test_dungeon_manager.py
+++ tests/unit/test_dungeon_manager.py
@@ -2,13 +2,26 @@ from brownie import DungeonManager, network
 from scripts.helpful_scripts import LOCAL_BLOCKAIN_ENVIRONMENTS, Gender, get_account, get_character, CharacterClass
 from scripts.deploy import ARTIST_FEE, callback_with_randomness, deploy_dungeon_manager, deploy_fantasy
 import pytest
+from web3 import Web3
 
+def test_create_dungeon():
+    if network.show_active() not in LOCAL_BLOCKAIN_ENVIRONMENTS:
+        pytest.skip()
+    fantasy = deploy_fantasy()
+    dm = deploy_dungeon_manager(fantasy_address=fantasy.address)
+    account = get_account()
+    treasure = Web3.toWei(1, "ether")
 
-# def test_create_dungeon():
-#     if network.show_active() not in LOCAL_BLOCKAIN_ENVIRONMENTS:
-#         pytest.skip()
-#     fantasy = deploy_fantasy()
-#     dm = deploy_dungeon_manager(fantasy_address=fantasy.address)
-#     account = get_account()
+    dm.createDungeon({"from": account, "value": treasure})
 
-#     dm.createDungeon({"from": account, "value": Web3.toWei(1, "ether")})
+    dungeon = dm.dungeons(account.address)
+    assert dungeon[0] == account.address
+    assert dungeon[1] == treasure
+
+    assert_adventuring_party_is_empty(dungeon[2])
+
+def assert_adventuring_party_is_empty(party):
+    assert party[0] == "0x0000000000000000000000000000000000000000"
+    assert len(party[1]) == 0
+    assert party[2] == 0
+

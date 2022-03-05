a7679917b5c0346582d06810aa5fba2f00d0dfbc
Anthalasath <chapchico@live.com>
2022-03-05 22:46:52 +0100

    add test for Retiring an active dungeon

--- test/fantasy-test.ts
+++ test/fantasy-test.ts
@@ -1,5 +1,6 @@
-import { ethers } from "hardhat";
+import { ethers, waffle } from "hardhat";
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { deployDungeonManager, deployFantasyWithDependencies } from "../scripts/deploy";
 
 // TODO: Only run unit tests in local blockchain
@@ -54,6 +55,34 @@ describe("Fantasy", () => {
         const dmWithSigner = dm.connect(account);
 
         expect(dmWithSigner.retireDungeon()).to.be.revertedWith("there is no dungeon belonging to this address");
+    });
+
+    it("Retiring an active dungeon should retire it", async () => {
+        const { fantasy, vrfCoordinatorV2, fantasyUtils } = await deployFantasyWithDependencies(true);
+        const dm = await deployDungeonManager({
+            fantasyAddress: fantasy.address,
+            vrfCoordinatorV2Address: vrfCoordinatorV2.address,
+            fantasyUtilsAddress: fantasyUtils.address
+        });
+        const accounts = await ethers.getSigners();
+        const account = accounts[0];
+        const dmWithSigner = dm.connect(account);
+        const treasure = ethers.utils.parseEther("1");
+        (await dmWithSigner.createDungeon({ value: treasure })).wait();
+        const balanceBeforeRetiringDungeon = await waffle.provider.getBalance(account.address);
+
+        const tx = await dmWithSigner.retireDungeon();
+        const receipt = await tx.wait();
+
+        const dungeonRetired = getEvent(receipt.events, "DungeonRetired");
+        expect(dungeonRetired.args.creator).to.equal(account.address);
+        expect(dungeonRetired.args.treasure).to.equal(treasure);
+        const dmBalance = await waffle.provider.getBalance(dmWithSigner.address);
+        expect(dmBalance).to.equal(0);
+        const accountBalance = await waffle.provider.getBalance(account.address);
+        expect(accountBalance.gt(balanceBeforeRetiringDungeon)).to.be.true;
+
+        expectDungeonDoesntExist(dm.dungeons(account.address));
     });
 });
 
